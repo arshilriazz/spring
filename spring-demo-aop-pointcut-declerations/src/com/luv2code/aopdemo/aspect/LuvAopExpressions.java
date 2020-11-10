66aad3715c11500025a52f47cccd2557b90fd0b3
Arshil Riaz <arshil.riaz@zemosolabs.com>
2020-11-10 13:12:36 +0530

    added simple maven

--- spring-demo-aop-pointcut-declerations/src/com/luv2code/aopdemo/aspect/LuvAopExpressions.java
+++ spring-demo-aop-pointcut-declerations/src/com/luv2code/aopdemo/aspect/LuvAopExpressions.java
@@ -8,16 +8,13 @@ public class LuvAopExpressions {
 
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	public void forDaoPackage() {}
-	
-	// create pointcut for getter methods
+
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
 	public void getter() {}
-	
-	// create pointcut for setter methods
+
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
 	public void setter() {}
 	
-	// create pointcut: include package ... exclude getter/setter
 	@Pointcut("forDaoPackage() && !(getter() || setter())")
 	public void forDaoPackageNoGetterSetter() {}
 
